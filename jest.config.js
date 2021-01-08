99b619829faffb3e25d7c11e54b914259112ae0c
Tom Sherman <the.tomsherman@gmail.com>
2021-01-08 22:43:31 +0000

    Add prettier and jest

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+/*
+ * For a detailed explanation regarding each configuration property and type check, visit:
+ * https://jestjs.io/docs/en/configuration.html
+ */
+
+module.exports = {
+  clearMocks: true,
+  testEnvironment: 'node',
+  modulePathIgnorePatterns: ['<rootDir>/common/deploy'],
+};
